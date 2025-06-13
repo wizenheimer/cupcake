8418add34bada9ecb717ea358b3593bf4731f965
wizenheimer <xNayanKumar@gmail.com>
2025-06-13 18:32:41 +0530

    build: bump release

--- dist/cupcake.rb
+++ dist/cupcake.rb
@@ -1,6 +1,6 @@
 cask "cupcake" do
-  version "1.0.0"
-  sha256 "cfe609ce54d3d0270cf639462bb7393b25ae95c572e7100baeea3f9c55aa7c52"
+  version "1.0.1"
+  sha256 "90ff13cea43063503c231b981b9abebe3728a29beb9821bd601a5b361a00686f"
 
   url "https://github.com/wizenheimer/cupcake/releases/download/v\#{version}/cupcake.zip"
   name "Cupcake"
@@ -10,7 +10,7 @@ cask "cupcake" do
   app "cupcake.app"
 
   caveats <<~EOS
-    ⚠️ This app is not signed or notarized.
+    This app is not signed or notarized.
     To open it the first time:
       1. Right-click the app in Finder
       2. Click "Open"
