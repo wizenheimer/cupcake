31ca288f0acdab4e96ca2e041675c921b058ee59
wizenheimer <xNayanKumar@gmail.com>
2025-06-13 15:54:30 +0530

    Initial Commit

---
+++ cupcake/cupcakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  cupcakeApp.swift
+//  cupcake
+//
+//  Created by Nayan on 13/06/25.
+//
+
+import SwiftUI
+
+@main
+struct cupcakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
